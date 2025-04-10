0990b882d2ca03ddec75a18fa4137fda6bc509f3
Gwen Miller <gwen.anne.miller@gmail.com>
2025-04-10 19:06:32 +0000

    fixed bug. now able to get the pnet preds and probs without dimension errors and save as a csv

--- src/report_and_eval.py
+++ src/report_and_eval.py
@@ -1,5 +1,5 @@
 """
-Functions for 
+Functions for
 - reporting information during running of a program, eg details about DFs
 - reporting evaluation information for models
 Author: Gwen Miller <gwen_miller@g.harvard.edu>
@@ -17,10 +17,10 @@ import json
 # Importing packages related to model performance
 # from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import (
-    confusion_matrix, # expects true_labels, predicted_labels
-    classification_report, # expects true_labels, predicted_labels
-    roc_auc_score, # expects true_labels, predicted_probs
-    average_precision_score, # aka the AUC-PRC score
+    confusion_matrix,  # expects true_labels, predicted_labels
+    classification_report,  # expects true_labels, predicted_labels
+    roc_auc_score,  # expects true_labels, predicted_probs
+    average_precision_score,  # aka the AUC-PRC score
     f1_score,
     precision_score,
     recall_score,
@@ -30,16 +30,14 @@ from sklearn.metrics import (
 )
 import torch
 
-logging.basicConfig(
-            format='%(asctime)s %(levelname)-8s %(message)s',
-            level=logging.INFO,
-            datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger()
 
 
 #################
 # Reporting
 #################
+
 
 def make_path_if_needed(file_path):
     directory = os.path.dirname(file_path)
@@ -48,7 +46,7 @@ def make_path_if_needed(file_path):
 
 
 def make_dir_if_needed(directory):
-    if not os.path.isdir(directory) and directory != '':
+    if not os.path.isdir(directory) and directory != "":
         logging.debug(f"Directory did not exist; making directory {directory}")
         os.makedirs(directory)
     return
@@ -58,12 +56,12 @@ def savefig(plt, save_path, png=True, pdf=True):
     make_path_if_needed(save_path)
     logging.info(f"saving plot to {save_path}")
     if png:
-        plt.savefig(save_path, bbox_inches='tight')
+        plt.savefig(save_path, bbox_inches="tight")
     if pdf:
-        plt.savefig(f"{save_path}.pdf", format="pdf", bbox_inches='tight')
+        plt.savefig(f"{save_path}.pdf", format="pdf", bbox_inches="tight")
 
 
-def report_df_info(*dataframes, n=5): # TODO: use this instead of load_df_verbose
+def report_df_info(*dataframes, n=5):  # TODO: use this instead of load_df_verbose
     """
     Report information about an arbitrary number of dataframes.
 
@@ -137,12 +135,19 @@ def report_df_info_with_names(df_dict, n=5):
 # Evaluation of model
 #################
 
-def get_loss_plot(train_losses, test_losses, 
-                  train_label="Train loss", test_label="Validation loss",
-                  title="Model Loss", ylabel="Loss", xlabel="Epochs"):
+
+def get_loss_plot(
+    train_losses,
+    test_losses,
+    train_label="Train loss",
+    test_label="Validation loss",
+    title="Model Loss",
+    ylabel="Loss",
+    xlabel="Epochs",
+):
     logging.info("Making a loss plot over time")
     # Sample data
-    epochs = range(1, len(train_losses)+1)
+    epochs = range(1, len(train_losses) + 1)
 
     # Plotting the lines
     plt.plot(epochs, train_losses, label=train_label)
@@ -156,14 +161,27 @@ def get_loss_plot(train_losses, test_losses,
     # Adding a legend
     plt.legend()
     return plt
-    
+
 
 def get_pnet_preds_and_probs(model, pnet_dataset):
-    model.to('cpu') # TODO: is this needed?
+    model.to("cpu")  # TODO: is this needed?
     x = pnet_dataset.x
     additional = pnet_dataset.additional
     pred_probas = model.predict_proba(x, additional).detach()
     preds = model.predict(x, additional).detach()
+
+    # Ensure preds is 1D
+    if preds.dim() > 1:
+        preds = preds.squeeze(1)  # Converts shape from [N, 1] → [N]
+
+    # Ensure pred_probas is 1D for binary classification
+    if pred_probas.dim() > 1 and pred_probas.shape[1] == 2:
+        pred_probas = pred_probas[:, 1]  # Take the probability of class 1
+    elif pred_probas.dim() > 1 and pred_probas.shape[1] == 1:
+        pred_probas = pred_probas.squeeze(1)  # Remove the singleton dimension; converts shape from [N, 1] → [N]
+    logging.info(f"Shape of preds: {preds.shape}")
+    logging.info(f"Shape of pred_probas: {pred_probas.shape}")
+
     return preds, pred_probas
 
 
@@ -171,7 +189,12 @@ def get_performance_metrics(who, y_trues, y_preds, y_probas, save_dir=None):
     """
     Get and log useful performance metrics for a given data split (designated by 'who' parameter)
     """
-    assert who in ['train', 'test', 'val', 'validation'], f"Expected one of train, test, val, or validation but got '{who}'"
+    assert who in [
+        "train",
+        "test",
+        "val",
+        "validation",
+    ], f"Expected one of train, test, val, or validation but got '{who}'"
 
     # TODO: add auc curve pdf as in Pnet.evaluate_interpret_save
     # logging.info(f"Logging {who} set ROC plot to W&B")
@@ -183,23 +206,23 @@ def get_performance_metrics(who, y_trues, y_preds, y_probas, save_dir=None):
         f"{who}_average_precision_score": average_precision_score(y_trues, y_probas),
         f"{who}_f1_score": f1_score(y_trues, y_preds),
         f"{who}_confusion_matrix\n": confusion_matrix(y_trues, y_preds).tolist(),
-        f"{who}_classification report\n": classification_report(y_trues, y_preds)
+        f"{who}_classification report\n": classification_report(y_trues, y_preds),
     }
 
     logging.info(f"{who} set metrics:")
-    for k,v in metric_dict.items():
+    for k, v in metric_dict.items():
         logging.info(f"{k}: {v}")
 
     if save_dir is not None:
         make_dir_if_needed(save_dir)
-        p = os.path.join(save_dir, f'{who}_performance_metrics.json')
+        p = os.path.join(save_dir, f"{who}_performance_metrics.json")
         logging.info(f"Saving dictionary of {who} set metrics to {p}")
-        with open(p, 'w') as json_file:
+        with open(p, "w") as json_file:
             json.dump(metric_dict, json_file)
-            
+
     # TODO: should I break the W&B pieces into a different function?
     logging.info(f"Saving {who} set metrics to W&B:")
-    for k,v in metric_dict.items():
+    for k, v in metric_dict.items():
         wandb.run.summary[k] = v
     logging.info(f"Logging {who} set confusion matrix plot to W&B")
     wandb.sklearn.plot_confusion_matrix(y_trues, y_preds)
@@ -211,7 +234,7 @@ def get_summary_metrics_wandb(model, x_train, y_train, x_test, y_test):
     logging.info("Logging plot summary metrics to W&B")
     wandb.sklearn.plot_summary_metrics(model, x_train, y_train, x_test, y_test)
     return
-    
+
 
 def get_train_test_manual_split(x, y, train_inds, test_inds):
     """
@@ -242,12 +265,11 @@ def get_train_test_manual_split(x, y, train_inds, test_inds):
     return X_train, X_test, y_train, y_test
 
 
-
-def get_model_preds_and_probs(model, who, model_type = "pnet", pnet_dataset=None, x=None, verbose=False):
+def get_model_preds_and_probs(model, who, model_type="pnet", pnet_dataset=None, x=None, verbose=False):
     logging.info(f"Model_type = {model_type}. Computing model predictions on {who} set")
-    if model_type == 'pnet':
+    if model_type == "pnet":
         y_preds, y_probas = get_pnet_preds_and_probs(model, pnet_dataset)
-    elif model_type in ['rf', 'bdt']:
+    elif model_type in ["rf", "bdt"]:
         y_preds, y_probas = get_sklearn_model_preds_and_probs(model, x)
     else:
         logging.error(f"We haven't implemented for the model type you specified, which was {model_type}")
@@ -255,7 +277,7 @@ def get_model_preds_and_probs(model, who, model_type = "pnet", pnet_dataset=None
         logging.info(f"Hist of model prediction probabilities on {who} set")
         plt.hist(y_probas)
         plt.show()
-    return  y_preds, y_probas
+    return y_preds, y_probas
 
 
 def get_sklearn_model_preds_and_probs(sklearn_model, x):
@@ -266,38 +288,72 @@ def get_sklearn_model_preds_and_probs(sklearn_model, x):
 
 def get_sklearn_feature_importances(sklearn_model, who, input_df, save_dir=None):
     importances = sklearn_model.feature_importances_
-    gene_feature_importances = pd.Series(importances, index=input_df.columns) # TODO: check if this is the correct index
+    gene_feature_importances = pd.Series(
+        importances, index=input_df.columns
+    )  # TODO: check if this is the correct index
     # TODO: edit so that it's a better format when saved down
-    logging.debug("Editing DF format so that we just have two columns to save down. This makes the read-in format much nicer.")
+    logging.debug(
+        "Editing DF format so that we just have two columns to save down. This makes the read-in format much nicer."
+    )
     gene_feature_importances = gene_feature_importances.reset_index()
-    gene_feature_importances.columns = ['feature', 'importance score']
+    gene_feature_importances.columns = ["feature", "importance score"]
     if save_dir is not None:
         make_dir_if_needed(save_dir)
         logging.info(f"Saving feature importance information to {save_dir}")
-        gene_feature_importances.to_csv(os.path.join(save_dir,f'{who}_gene_feature_importances.csv'), index=False)
+        gene_feature_importances.to_csv(os.path.join(save_dir, f"{who}_gene_feature_importances.csv"), index=False)
         # wandb.save(f'{who}_gene_feature_importances.csv', base_path=save_dir, policy="end") # TODO: problem. save_dir is above current dir, and this isn't allowed.
     return gene_feature_importances
 
 
-def get_pnet_feature_importances(model, who, pnet_dataset, save_dir = None):
+def get_pnet_feature_importances(model, who, pnet_dataset, save_dir=None):
     """
     Args:
     - model: this is a Pnet model object
     - who: train, test, validation, or val. Which dataset are you using?
     - pnet_dataset: this is a Pnet dataset object (not a DF), and has attributes x, additional, y, etc.
     """
-    gene_feature_importances, additional_feature_importances, gene_importances, layer_importance_scores = model.interpret(pnet_dataset)
-    
+    gene_feature_importances, additional_feature_importances, gene_importances, layer_importance_scores = (
+        model.interpret(pnet_dataset)
+    )
+
     if save_dir is not None:
         make_dir_if_needed(save_dir)
         logging.info(f"Saving feature importance information to {save_dir}")
-        gene_feature_importances.to_csv(os.path.join(save_dir, f'{who}_gene_feature_importances.csv'))
-        additional_feature_importances.to_csv(os.path.join(save_dir, f'{who}_additional_feature_importances.csv'))
-        gene_importances.to_csv(os.path.join(save_dir, f'{who}_gene_importances.csv'))
+        gene_feature_importances.to_csv(os.path.join(save_dir, f"{who}_gene_feature_importances.csv"))
+        additional_feature_importances.to_csv(os.path.join(save_dir, f"{who}_additional_feature_importances.csv"))
+        gene_importances.to_csv(os.path.join(save_dir, f"{who}_gene_importances.csv"))
         for i, layer in enumerate(layer_importance_scores):
-            layer.to_csv(os.path.join(save_dir, '{}_layer_{}_importances.csv'.format(who, i)))
+            layer.to_csv(os.path.join(save_dir, "{}_layer_{}_importances.csv".format(who, i)))
 
     return gene_feature_importances, additional_feature_importances, gene_importances, layer_importance_scores
+
+
+def save_predictions_and_probs(save_dir, who, y_true, y_preds, y_probas, indices=None):
+    """
+    Save predictions, probabilities, true labels, and optionally sample indices for analysis.
+
+    Parameters:
+        save_dir (str): Directory to save the results.
+        who (str): Identifier for the dataset (e.g., 'test', 'validation').
+        y_true (array-like): True labels.
+        y_preds (array-like): Predicted labels.
+        y_probas (array-like): Predicted probabilities.
+        indices (array-like, optional): Indices of the samples.
+    """
+    results_dict = {
+        "true": y_true.squeeze().numpy(),
+        "predicted": y_preds.squeeze().numpy(),
+        "probability": y_probas.squeeze().numpy(),
+    }
+
+    if indices is not None:
+        results_dict["index"] = indices
+
+    results_df = pd.DataFrame(results_dict)
+    results_path = os.path.join(save_dir, f"{who}_predictions_and_probs.csv")
+    results_df.to_csv(results_path, index=False)
+    logging.info(f"Saved predictions, probabilities, and indices to {results_path}.")
+    return results_df
 
 
 def evaluate_interpret_save(model, who, model_type, pnet_dataset=None, x=None, y=None, save_dir=None):
@@ -308,27 +364,47 @@ def evaluate_interpret_save(model, who, model_type, pnet_dataset=None, x=None, y
     if save_dir is not None:
         make_dir_if_needed(save_dir)
         logging.info(f"Results will be saved to {save_dir}.")
-    
+
     # TODO: need a universal way to get the X vs y components of the `pnet_dataset`
-    if model_type == 'pnet':
+    if model_type == "pnet":
         y = pnet_dataset.y
-        logging.info(f"Getting the {model_type} model predictions on the {who} set, performance metrics, and feature importances (if applicable)")
-        y_preds, y_probas = get_model_preds_and_probs(model=model, pnet_dataset=pnet_dataset, who=who, model_type=model_type)
-        metric_dict = get_performance_metrics(who, y, y_preds, y_probas, save_dir) # TODO: this is universal, and should get pulled out   
-        gene_feature_importances, additional_feature_importances, gene_importances, layer_importance_scores = get_pnet_feature_importances(model, who, pnet_dataset, save_dir)
+        logging.info(
+            f"Getting the {model_type} model predictions on the {who} set, performance metrics, and feature importances (if applicable)"
+        )
+        y_preds, y_probas = get_model_preds_and_probs(
+            model=model, pnet_dataset=pnet_dataset, who=who, model_type=model_type
+        )
+        save_predictions_and_probs(
+            save_dir, who, y, y_preds, y_probas, indices=pnet_dataset.input_df.index.tolist()
+        )  # TODO: this is universal, and should get pulled out
+        metric_dict = get_performance_metrics(
+            who, y, y_preds, y_probas, save_dir
+        )  # TODO: this is universal, and should get pulled out
+
+        gene_feature_importances, additional_feature_importances, gene_importances, layer_importance_scores = (
+            get_pnet_feature_importances(model, who, pnet_dataset, save_dir)
+        )
         importances = {
-            'gene_feature_importances':gene_feature_importances, 
-            'additional_feature_importances':additional_feature_importances, 
-            'gene_importances':gene_importances, 
-            'layer_importance_scores':layer_importance_scores,
+            "gene_feature_importances": gene_feature_importances,
+            "additional_feature_importances": additional_feature_importances,
+            "gene_importances": gene_importances,
+            "layer_importance_scores": layer_importance_scores,
         }
         # for name,dat in importances.items():
         #     save_as_file_to_wandb(dat, f'{who}_{name}.csv')
-            # wandb.run.summary[k] = v.to_dict() # TODO: haven't tested and I expect it might not work for everything, some some might be a list?
-        return metric_dict, gene_feature_importances, additional_feature_importances, gene_importances, layer_importance_scores
+        # wandb.run.summary[k] = v.to_dict() # TODO: haven't tested and I expect it might not work for everything, some some might be a list?
+        return (
+            metric_dict,
+            gene_feature_importances,
+            additional_feature_importances,
+            gene_importances,
+            layer_importance_scores,
+        )
 
-    elif model_type in ['rf', 'bdt']:
-        logging.info(f"Getting the {model_type} model predictions on the {who} set, performance metrics, and feature importances (if applicable)")
+    elif model_type in ["rf", "bdt"]:
+        logging.info(
+            f"Getting the {model_type} model predictions on the {who} set, performance metrics, and feature importances (if applicable)"
+        )
         x = pnet_dataset.x
         y = pnet_dataset.y.ravel()
         input_df = pnet_dataset.input_df
@@ -337,24 +413,24 @@ def evaluate_interpret_save(model, who, model_type, pnet_dataset=None, x=None, y
         # If the positive class is class 1, use the probabilities for that class
         y_probas = y_probas[:, 1]
 
-        metric_dict = get_performance_metrics(who, y, y_preds, y_probas, save_dir)    
+        metric_dict = get_performance_metrics(who, y, y_preds, y_probas, save_dir)
         gene_feature_importances = get_sklearn_feature_importances(model, who=who, input_df=input_df, save_dir=save_dir)
         # TODO: trying different ways of saving the feature importances (as a dict, a file, and as two separate lists)
         # save_as_file_to_wandb(gene_feature_importances, f'{who}_gene_feature_importances.csv')
         # gene_feature_importances= gene_feature_importances.to_dict()
         # print(f"gene_feature_importances: {gene_feature_importances}")
         # TODO: why isn't this working with the validation set? Works fine with the training set
-        # wandb.run.summary['gene_feature_importances'] = gene_feature_importances 
+        # wandb.run.summary['gene_feature_importances'] = gene_feature_importances
         # wandb.run.summary['gene_feature_importances_names'] = list(gene_feature_importances.keys())
         # wandb.run.summary['gene_feature_importances_values'] = list(gene_feature_importances.values())
         return gene_feature_importances
-    
+
     else:
         logging.error(f"We haven't implemented for the model type you specified, which was {model_type}")
     return
 
 
-def save_as_file_to_wandb(data, filename, policy='now', delete_local=True):
+def save_as_file_to_wandb(data, filename, policy="now", delete_local=True):
     logging.info(f"Temporarily save down to {filename}, upload to WandB.")
     if not isinstance(data, pd.DataFrame):
         logging.warn(f"Expected a DF as input; converting {type(data)} object to pandas DF before saving.")
@@ -369,7 +445,7 @@ def save_as_file_to_wandb(data, filename, policy='now', delete_local=True):
 
 def get_deviance(clf, x_test, y_test):
     """
-    TODO: check functionality for RF. Works for BDT. 
+    TODO: check functionality for RF. Works for BDT.
     Thinking about the "Plot training deviance" section from https://scikit-learn.org/stable/auto_examples/ensemble/plot_gradient_boosting_regression.html#sphx-glr-auto-examples-ensemble-plot-gradient-boosting-regression-py
     """
     test_score = np.zeros((clf.n_estimators_,), dtype=np.float64)
